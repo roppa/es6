4bfc910ea6d0e8123f82999a0ba0dd5cdb57f11a
Mark Robson <roppa_uk@hotmail.com>
2015-11-06 16:23:37 +0000

    Added setPrototypeOf example

--- modules/object.js
+++ modules/object.js
@@ -1,3 +1,22 @@
 "use strict";
 
 module.exports = exports = {};
+
+//Notes: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/proto
+//Object.prototype.__proto__ is part of the es6 spec, but it is recommended not to use it. 
+
+/**
+ * Example of the SetPrototypeOf static method. 
+ * @param  {object} obj   [description]
+ * @param  {} proto [description]
+ * @return {[type]}       [description]
+ */
+exports.setPrototype = (obj, proto) => {
+  Object.setPrototypeOf(obj, proto);
+  obj.constructor();
+};
+
+
+//Object.is()
+
+
